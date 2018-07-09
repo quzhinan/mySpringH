4768c223656d8fe0e793bf8070182d7bea090aac
quzhinan <444562213@qq.com>
2018-07-09 11:41:00 +0800

    test

--- WebContent/WEB-INF/views/test.jsp
+++ WebContent/WEB-INF/views/test.jsp
@@ -1,15 +1,20 @@
-<html>
-    <script>  
-    	var click111 = function(){
-    		alert(1);
-    		click222();
-    	}
-    	var click222 = function(){
-    		alert(2)
-    	}
-    
-    </script>  
-    <div id="div1">
-    <button id="button" onclick="click111()">click</button>
-    </div>
-</html>  
+<script>
+	function click111() {
+		callAjax("http://172.20.15.132:8082/JMODE_ASP/faces/aOrderSumServlet", "", "POST", false, "xml", function(data){
+			/* console.log(data)
+			sessionStorage.setItem("data",JSON.stringify(data.data)); 
+			$("#div2").text(sessionStorage.getItem("data"))*/
+			/* $("#img1").attr("src","data:image/gif;" + data); */
+			alert(data)
+		})
+	}
+	
+</script>
+
+<div id="div1">
+<button id="button" onclick="click111()">click</button>
+</div>
+<div id="div2">
+<img id="img1" src="">
+</div>
+<canvas id="canvas"></canvas>
--- src/main/com/qzn/servlets/TestServlet.java
+++ src/main/com/qzn/servlets/TestServlet.java
@@ -2,7 +2,14 @@ package com.qzn.servlets;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -36,23 +43,19 @@ public class TestServlet extends ServletProxy {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String url = "http://www.town.aikawa.kanagawa.jp/feed.xml?type=rss_2.0&new1=1";
+//		String jsonUrl = "http://www.town.aikawa.kanagawa.jp/feed.xml?type=rss_2.0&new1=1";
+		String fileUrl = "/Users/quzhinan/datas/pic/722a0002eec58da7ccb9.gif";
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "get,post");
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with,content-type");
-		getXmlFile(url, response);
+//		readJson(jsonUrl, response);
+		readFileFromLocal(fileUrl, response);
 	}
 
-	public String getXmlFile(String url, HttpServletResponse response) {
-		// 创建httpclient工具对象
+	public void readFileFromUrl(String url, HttpServletResponse response) {
 		HttpClient client = new HttpClient();
-		// 创建post请求方法
 		PostMethod myPost = new PostMethod(url);
-		String responseString = null;
 		try {
-			// 设置请求头部类型
-			myPost.setRequestHeader("Content-Type", "application/xml");
-			myPost.setRequestHeader("charset", "UTF-8");
 			int statusCode = client.executeMethod(myPost);
 			if (statusCode == HttpStatus.SC_OK) {
 				BufferedInputStream bis = new BufferedInputStream(myPost.getResponseBodyAsStream());
@@ -71,7 +74,63 @@ public class TestServlet extends ServletProxy {
 			e.printStackTrace();
 		}
 		myPost.releaseConnection();
-		return responseString;
+	}
+	
+	public void readFileFromLocal(String url, HttpServletResponse response) {
+		
+		File file = new File(url);
+		try {
+			FileInputStream fis = new FileInputStream(file);
+			BufferedInputStream bis = new BufferedInputStream(fis);
+			byte[] bytes = new byte[1024];
+			ServletOutputStream os = response.getOutputStream();
+			BufferedOutputStream bos = new BufferedOutputStream(os);
+			int count = 0;
+			while ((count = bis.read(bytes)) != -1) {
+				bos.write(bytes, 0, count);
+			}
+			bos.flush();
+			bos.close();
+			bis.close();
+			fis.close();
+//			byte[] data = null;
+//			data = new byte[fis.available()];
+//			fis.read(data);
+//			fis.close();
+//			BASE64Encoder encoder = new BASE64Encoder();
+//			String temPath = encoder.encode(data);
+//			String retPath = "base64," + temPath;
+//			response.getWriter().write(retPath);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void readJson(String url, HttpServletResponse response) {
+		
+		HttpURLConnection connection = null;
+		InputStreamReader isr = null;
+		OutputStreamWriter osw = null;
+		try {
+			URL targetUrl = new URL(url);
+			connection = (HttpURLConnection) targetUrl.openConnection();
+			connection.setRequestMethod("POST");
+			
+			if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+				isr = new InputStreamReader(connection.getInputStream(), "UTF-8");
+				osw = new OutputStreamWriter(response.getOutputStream(), "UTF-8");
+				BufferedReader br = new BufferedReader(isr);
+				String line = null;
+				while ((line = br.readLine()) != null) {
+					osw.write(line);
+				}
+				osw.flush();
+				osw.close();
+				isr.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
